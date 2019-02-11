a8d09f5407c2b41dec1fd9096307d235e2f5f8a7
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2019-02-11 16:07:25 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     hostname = socket.gethostname()
-    return render_template('profile.html', hostname=hostname)
+    return render_template('profiles.html', hostname=hostname)
 
 if __name__ == "__main__":
     application.run()
